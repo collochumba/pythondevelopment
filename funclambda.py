8ffb2977cd06870c2f198ce98a075a52cb8f6b42
collochumba <chumbasea@gmail.com>
2023-05-25 20:21:23 +0300

    updated changes

---
+++ funclambda.py
@@ -0,0 +1,5 @@
+def polynomial (x):
+    return x**2 + 5*x + 4
+print(polynomial(-7))# returns the value of polynomial of 4 #0
+# lambda creates function on fly way# hehe instantly
+print((lambda x: x**2 + 5*x + 4)(-7)) # outputs 0

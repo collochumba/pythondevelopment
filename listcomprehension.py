81abaec13a7473b39fa18906176c8532bfe60ae4
collochumba <chumbasea@gmail.com>
2023-05-25 19:10:04 +0300

    a program to print the lengt of strings in a list

---
+++ listcomprehension.py
@@ -0,0 +1,5 @@
+# a program to print the lengt of strings in a list
+l = ["abcd","abcde","collins"]
+l2 = [len(value)for value in l]
+print("this is the list and length is below:",l)
+print("length of each item in the list:",l2)

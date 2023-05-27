7cadda37831c45ea8a8c727620ab71d47fb6dd79
collochumba <chumbasea@gmail.com>
2023-05-27 15:22:51 +0300

    show the write operation of files in python

---
+++ fileoper1.py
@@ -0,0 +1,6 @@
+#in write mode is used to create new files
+#if you write on already existing file you will lose its contents
+fp = open("samplwritefile","w")
+fp.write()
+#a new file is added of the bottom left as show in pic below
+#check for [samplewritefile u]

ae8310a81a506bc418edabdc31962094b9eeda15
collochumba <chumbasea@gmail.com>
2023-05-29 19:10:04 +0300

    player

---
+++ player.py
@@ -0,0 +1,23 @@
+class Player:
+    def _init_(self, name, lives):
+        self.name = name
+        self._lives = lives
+
+    def hit(self):
+        self._lives -= 1
+    
+    #your code goes here
+    @property
+    def isAlive (self ):
+      if self.lives > 0:
+       return True
+
+p = Player("Cyberpunk77", int(input()))
+i = 1
+while True:
+    p.hit()
+    print("Hit # " + str(i))
+    i += 1
+    if not p.isAlive:
+        print("Game Over")
+        break

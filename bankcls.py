bfdd906cfdd51eabb682591992fcd4d43dcf7ba7
collochumba <chumbasea@gmail.com>
2023-05-29 19:37:56 +0300

    creations of methods in a class

---
+++ bankcls.py
@@ -0,0 +1,13 @@
+class account:# declaration of a class
+    def __init__(self,cusid,name,accbalance = 0):#constructor to create instance
+        self.cusid = cusid
+        self.name = name
+        self.accbalance = accbalance
+    def balance(self):
+        return self.accbalance
+    def deposit(self, amount):
+        self.accbalance = self.accbalance + amount
+        return self.accbalance
+customer1 = account(101,"collins")
+print("The intial balance:",customer1.balance())
+print("brings the updated balance:",customer1.deposit(5000))

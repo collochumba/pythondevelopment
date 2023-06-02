37703c05426b2454ee290b74f8ece1e1f163aa1c
collochumba <chumbasea@gmail.com>
2023-06-02 16:25:02 +0300

    selecting to view database

--- db2handling.py
+++ db2handling.py
@@ -2,7 +2,6 @@ import sqlite3
 conn = sqlite3.connect('example1.db')
 c = conn.cursor()
 #this how you view the contents on the table
-
 c.execute("""SELECT * FROM details""")
 for row in c:
     print(row)#prints the details in the table details in a row
